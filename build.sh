39b906ca1e2f9821a52667420a965f7f97830bd3
zyx <zz593141477@gmail.com>
2019-02-17 11:07:19 +0800

    repack

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#python core_build_list_generator.py
+python core_build_list_generator.py
 node node_compress_generator.js
 node node_compress_utility.js
 node node_compress_block.js
 node node_compress_core.js
-mv node_compressed_*.js blockly_compressed.js release/
+mv node_compressed_*.js release/
 cp vs.css release/
