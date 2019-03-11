8ca1b63cc179ede1ef6b856752fedc91bf568888
z4yx <z4yx@users.noreply.github.com>
2019-03-11 11:12:34 +0800

    new release

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#python core_build_list_generator.py
+python core_build_list_generator.py
 node node_compress_generator.js
 node node_compress_utility.js
 node node_compress_block.js
-#node node_compress_core.js
+node node_compress_core.js
 mv node_compressed_*.js release/
+mv blockly_compressed.js release/
 cp vs.css release/
