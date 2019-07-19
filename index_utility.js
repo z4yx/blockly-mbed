abc77d28a6c5a6afc80006b7c85ca682fa71b98b
z4yx <z4yx@users.noreply.github.com>
2019-07-19 17:08:46 +0800

    double-click to select all

--- index_utility.js
+++ index_utility.js
@@ -368,3 +368,19 @@ function init() {
     load_by_url(dest);
   }
 }
+
+function selectElementText(el, win) {
+  win = win || window;
+  var doc = win.document, sel, range;
+  if (win.getSelection && doc.createRange) {
+      sel = win.getSelection();
+      range = doc.createRange();
+      range.selectNodeContents(el);
+      sel.removeAllRanges();
+      sel.addRange(range);
+  } else if (doc.body.createTextRange) {
+      range = doc.body.createTextRange();
+      range.moveToElementText(el);
+      range.select();
+  }
+}
